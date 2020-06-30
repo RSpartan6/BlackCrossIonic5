de9a86d23dbbeac78c21d4790c0168456c24c345
MiguelVJ <miguel.vaz.jim@hotmail.com>
2020-06-30 11:21:08 -0500

    modificaciones registro

--- src/app/paginas/registro/registro.page.ts
+++ src/app/paginas/registro/registro.page.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from 'src/app/servicios/login.service';
 import { Router } from '@angular/router';
-import { ToastController, LoadingController } from '@ionic/angular';
+import { ToastController, LoadingController, AlertController } from '@ionic/angular';
 import { HttpClient } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -11,56 +12,73 @@ import { HttpClient } from '@angular/common/http';
 })
 export class RegistroPage implements OnInit {
 
-  nu={
+  nu = {
     "idRol": "",
-    "usuario":"",
-    "contrasenia":"",
-    "nombre":"",
-    "sexo":"",
-    "correo_electronico":"",
-    "peso":"",
-    "altura":"--",
-    "imc":"--",
-    "telefono":"",
+    "usuario": "",
+    "contrasenia": "",
+    "nombre": "",
+    "sexo": "",
+    "correo_electronico": "",
+    "peso": "",
+    "altura": "--",
+    "imc": "--",
+    "telefono": "",
     "nivel": "",
-    "estatus":"",
-    "intentos":"",
+    "estatus": "",
+    "intentos": "",
   }
 
+  submitted = false;
+
   constructor
-  (
-    private router: Router, 
-    public toastController: ToastController, 
-    private servicio: LoginService,
-    public loadingController: LoadingController,
-    public http: HttpClient,
+    (
+      private router: Router,
+      public toastController: ToastController,
+      private servicio: LoginService,
+      public loadingController: LoadingController,
+      public http: HttpClient,
+      public alertController: AlertController
   ) { }
 
   // Agregar nuevo usuario
-  nuevoUsuario(){
+  nuevoUsuario(form : NgForm) {
     let obj = {
       // "idUsuario":null,
       "idRol": 2,
-      "usuario":this.nu.usuario,
-      "contrasenia":this.nu.contrasenia,
-      "nombre":this.nu.nombre,
-      "sexo":this.nu.sexo,
-      "correoElectronico":this.nu.correo_electronico,
-      "peso":0,
-      "altura":0,
-      "imc":"--",
-      "telefono":this.nu.telefono,
-      "nivel": "10" ,
-      "estatus":"1",
-      "intentos":0,
+      "usuario": this.nu.usuario,
+      "contrasenia": this.nu.contrasenia,
+      "nombre": this.nu.nombre,
+      "sexo": this.nu.sexo,
+      "correoElectronico": this.nu.correo_electronico,
+      "peso": 0,
+      "altura": 0,
+      "imc": "--",
+      "telefono": this.nu.telefono,
+      "nivel": "10",
+      "estatus": this.nu.estatus,
+      "intentos": 0,
     }
 
-    this.presentLoading();
-    this.servicio.setCrear(obj).subscribe((response: any) => {
-          console.log(response);
+    this.submitted = true;
+
+    if(form.valid){
+
+      this.servicio.setCrear(obj).subscribe((response: any) => {
+        console.log(response, "SetCrear Method");
+  
+        if (response.codigo == 500) {
+  
+          this.usuarioExistente()
+        }else {
+          this.presentLoading();
+        }
       });
-    console.log(this.nu);
-    // this.navCtrl.push(ClasesPage)
+      console.log(this.nu);
+      // this.navCtrl.push(ClasesPage)
+
+    }else{
+      this.todoslosCampos()
+    }
   }
 
   async presentLoading() {
@@ -74,6 +92,29 @@ export class RegistroPage implements OnInit {
     const { role, data } = await loading.onDidDismiss();
 
     this.router.navigate(['/perfil']);
+  }
+
+  async usuarioExistente() {
+    const loading = await this.loadingController.create({
+      cssClass: 'my-custom-class',
+      message: 'Usuario ya registrado',
+      duration: 500,
+    });
+    await loading.present();
+
+    const { role, data } = await loading.onDidDismiss();
+
+    this.router.navigate(['/registro']);
+  }
+
+  async todoslosCampos() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Todos los campos son necesarios',
+      buttons: ['OK']
+    });
+
+    await alert.present();
   }
 
   ngOnInit() {
